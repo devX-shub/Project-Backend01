baf2a1730dca75bae05e28673e46e86a37603e71
devX-shub <161881953+devX-shub@users.noreply.github.com>
2024-03-13 22:05:37 +0530

    added data function

---
+++ src/utils/cloudinary.js
@@ -0,0 +1,27 @@
+import {v2 as cloudinary} from 'cloudinary';
+import { response } from 'express';
+import { fs } from 'file-system';
+
+cloudinary.config({ 
+  cloud_name: process.env.CLOUD_NAME, 
+  api_key: process.env.CLOUD_API_KEY, 
+  api_secret: process.env.CLOUD_API_SECRATE 
+});
+
+const uploadOnCloud = async (loacalFilePath) => {
+    try {
+        if(!loacalFilePath) return null
+        //upload file
+        const response = await cloudinary.uploader.upload(loacalFilePath,{
+            resource_type : "auto"
+        })
+        //file Uploaded
+        console.log("file uploaded",response.url);
+        return response
+    } catch (error) {
+        fs.unlinkSync(loacalFilePath)  // remove local saved temp file as the upload failed
+        return null;
+    }
+}
+
+export {uploadOnCloud}
