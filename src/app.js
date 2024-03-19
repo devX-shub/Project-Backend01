111e567b54a20cd8b231ecf5b24f9ad02a317105
devX-shub <161881953+devX-shub@users.noreply.github.com>
2024-03-19 11:46:03 +0530

    route Basic done

--- src/app.js
+++ src/app.js
@@ -15,4 +15,12 @@ app.use(express.static("public"))
 app.use(cookieParser())
 
 
+//routes
+
+import userRouter from "./routes/user.routes.js"
+
+//routes declration
+//http://localhost:8080/user/register
+app.use("/api/v1/users",userRouter)
+
 export {app}
--- src/db/index.js
+++ src/db/index.js
@@ -1,10 +1,11 @@
 import mongoose from "mongoose";
 import { DB_NAME } from "../constants.js";
-
+const url = process.env.MONGO_URI
+console.log(url);
 const connectDB = async() => {
     try {
         const connectionInstance = await mongoose.connect(`${process.env.MONGO_URI}${DB_NAME}`)
-        console.log("Finally connect established");
+        console.log(`Finally connect established ${connectionInstance}`);
     } catch (error) {
         console.error("connection error : Unable to connect to mongodb ", error);
         process.exit(1);
